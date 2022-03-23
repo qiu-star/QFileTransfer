76ae8276ada2afc96cfa0d94b35fff02f7990828
qiu <ql123ql123@163.com>
2022-03-23 15:42:55 +0800

    登录完成

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import socket
-import ssl,time,os,struct,json,tkinter
+import ssl, time, os, struct, json, tkinter
 
 class Client:
     def __init__(self):
@@ -12,11 +12,56 @@ class Client:
         self.__sock = socket.create_connection(('127.0.0.1', 9999))
         self.__ssock = context.wrap_socket(self.__sock, server_hostname='SERVER', server_side=False)
     
-    def send(self):
-        text = "Hello World!\n"
-        buf = struct.pack('1024s', bytes(text.encode('utf-8')))
-        self.__ssock.send(buf)
+    def send_header(self, header, hformat):
+        header_hex = bytes(json.dumps(header).encode('utf-8'))
+        fhead = struct.pack(hformat, header_hex)
+        self.__ssock.send(fhead)
+        print('send over...')
+
+    def receive_file(self, file_size, file_name):
+        print('file name: %s, filesize: %s' % (file_name, file_size))
+        recvd_size = 0
+        file = open(file_name, 'wb')
+        print('start receiving...')
+        while not recvd_size == file_size:
+            if file_size - recvd_size > 1024:
+                rdata = self.__ssock.recv(1024)
+                recvd_size += len(rdata)
+            else:
+                rdata = self.__ssock.recv(file_size - recvd_size)
+                recvd_size = file_size
+            file.write(rdata)
+        file.close()
+        print('receive done')
+
+    def login(self, username, password):
+        header = {
+            'Command': 'Login',
+            'fileName': '',
+            'fileSize': '',
+            'time': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+            'user': username,
+            'password': password,
+        }
+        self.send_header(header,'1024s')
+        # wait for the server answer
+        fileinfo_size = struct.calcsize('128s')
+        buf = self.__ssock.recv(fileinfo_size)
+        if not buf:
+            return False
+        header_json = str(struct.unpack('128s', buf)[0], encoding='utf-8').strip('\00')
+        header = json.loads(header_json)
+        # from the server answer, we will know whether login successfully or not
+        stat = header['stat']
+        if stat != 'Success':
+            return False
+        # from the server answer, we will know the filesize of filenames in server
+        file_size = header['fileSize']
+        file_name = os.path.join('./client_cache/', 'file_catalogue.txt')
+        self.receive_file(file_size, file_name)
+        return True
+        
 
 if __name__ == "__main__":
     client = Client()
-    client.send()
+    client.login("qiu", "123")
--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 import socket
-import ssl, threading, struct
+import ssl, threading, struct, json, os, pymysql
 
 class Server:
     def __init__(self):
-         # generate ssl context
+        # open DB
+        self.__db = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='ql782134193', db='fileTransfer', charset='utf8')
+        self.__cursor = self.__db.cursor()
+        # generate ssl context
         self.__context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
         self.__context.load_verify_locations('./cer/CA/ca.crt')
         self.__context.load_cert_chain('./cer/server/server.crt', './cer/server/server_rsa_private.pem')
@@ -21,15 +24,92 @@ class Server:
                     thread = threading.Thread(target=self.deal_conn_thread, args=(connection,))
                     thread.start()
     
+    def send_header(self, connection, header, hformat):
+        header_hex = bytes(json.dumps(header).encode('utf-8'))
+        fhead = struct.pack(hformat, header_hex)
+        connection.send(fhead)
+        print('send over...')
+
+    def send_file(self, connection, file_name):
+        file_object = open(file_name, 'rb')
+        while True:
+            file_data = file_object.read(1024)
+            if not file_data:
+                break
+            connection.send(file_data)
+        file_object.close()
+
+    def write_log(self, log_txt):
+        with open('./server_cache/server_log.txt', 'a', encoding='utf-8') as log_file:
+            log_file.write(log_txt)
+
+    def login_succ(self, connection, username):
+        # when client login successfully, server will send the filenames on the server to the client
+        file_catalogue = './server_cache/file_catalogue.txt'
+        header = {
+            'Feedback': 'Login',
+            'stat': 'Success',
+            'fileSize': os.stat(file_catalogue).st_size,
+            'user': username
+        }
+        self.send_header(connection, header, '128s')
+        # send the filenames on the server to the client
+        self.send_file(connection, file_catalogue)
+
+    def login_fail(self, connection, username):
+        header = {
+            'Feedback': 'Login',
+            'stat': 'Fail',
+            'fileSize': '',
+            'user': username
+        }
+        self.send_header(connection, header, '128s')
+
+    def login(self, connection, header):
+        username = header['user']
+        password = header['password']
+        time = header['time']
+        # check whether the user and password is correct
+        sql = "select * from user where username = '%s' and password = '%s'"%(username,password)
+        self.__cursor.execute(sql)
+        data = self.__cursor.fetchone()
+        login_log = ""
+        if data:
+            self.login_succ(connection, username)
+            login_log = '\n%s try to login at "%s" , Stat: Success ' %(username, time)
+        else:
+            print("Fail")
+            login_log = '\n%s try to login at "%s" , Stat: Fail ' %(username, time)
+        # write log
+        self.write_log(login_log)
+
+    def do_command(self, connection, header):
+        command = header['Command']
+        if command == 'Login':
+            self.login(connection, header)
+        elif command == 'Register':
+            print('Register')
+        elif command == 'Upload':
+            print('Upload')
+        elif command == 'Download':
+            print('Download')
+        elif command == 'DeleteFile':
+            print('DeleteFile')
+        elif command == 'DeleteUser':
+            print('DeleteUser')
+        
+
     def deal_conn_thread(self, connection):
         while True:
             try:
                 connection.settimeout(60)
                 fileinfo_size = struct.calcsize('1024s')
                 buf = connection.recv(fileinfo_size)
-                if buf is None:
+                if not buf:
                     continue
-                print(buf)
+                header_json = str(struct.unpack('1024s', buf)[0], encoding='utf-8').strip('\00')
+                header = json.loads(header_json)
+                self.do_command(connection, header)
             except socket.timeout:
                 connection.close()
                 break
